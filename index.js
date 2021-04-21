03988ac5985e185206f49683b67b3a7adf6d87d4
vanessuniq <vanessuniq@gmail.com>
2021-04-21 18:32:59 -0400

    commit tree 47b53ca352e855f5681fe715d1b24a02c05b255f

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function bfs(rootNode, vertices, edges){
+function bfs(rootNode, vertices, edges) {
+    let exploredNodes = [],
+        queu = [rootNode];
+    while (queu.length !== 0) {
+        const node = queu.shift();
+        if (node.distance === null) {
+            node.distance = 0;
+        };
+        const adjacents = findAdjacent(node.name, vertices, edges);
+        markDistanceAndPredecessor(node, adjacents);
+        queu.push(...adjacents);
+        exploredNodes.push(node);
+    };
+    return exploredNodes;
+};
 
+function findAdjacent(node, vertices, edges) {
+    const adjacentNames = edges.filter(edge => edge.includes(node)).flat().filter(n => n !== node);
+    return vertices.filter(vertex => (adjacentNames.includes(vertex.name) && vertex.distance === null));
+};
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+    adjacentNodes.forEach(adjacentNode => {
+        adjacentNode.distance = node.distance + 1;
+        adjacentNode.predecessor = node;
+    });
 }
